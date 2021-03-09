180307dcfc02bfec45b4e36af30d9c6ba1ec2c6b
IliasBoutouil <boutouil.ilias@gmail.com>
2021-03-09 21:27:20 +0100

    mar.  9 mars 2021 21:27:19

--- Atelier1/Atelier1/Compte.cs
+++ Atelier1/Atelier1/Compte.cs
@@ -29,7 +29,7 @@ namespace Atelier1
         {
             Console.WriteLine("Compte Numero : "+id+"\nPropriétaire :");
             this.titulaire.Afficher();
-            Console.WriteLine("Solde : " + solde);
+            Console.WriteLine("Solde : " + solde.ToString());
         }
         public bool Créditer(MAD M)
         {
@@ -37,9 +37,15 @@ namespace Atelier1
             if (M > s)
             {
                 this.solde += M;
+                Console.WriteLine("Opération effectuer avec succes nouveau solde est  : "+solde.ToString());
                 return true;
             }
-            return false;
+            else
+            {
+                Console.WriteLine("Entrez un montant positif");
+                return false;
+            }
+            
         }
         public bool Débiter(MAD montant)
         {
@@ -49,15 +55,21 @@ namespace Atelier1
                 if (montant < this.solde)
                 {
                     this.solde -= montant;
+                    Console.WriteLine("Opération effectuer avec succes nouveau solde est  : " + solde.ToString());
                     return true;
                 }
                 else
                 {
+                    Console.WriteLine("Entrez un montant inférieur à "+solde.ToString());
                     return false;
                 }
             }
             else
+            {
+                Console.WriteLine("Entrez un montant positif inférieur à " + solde.ToString());
                 return false;
+            }
+               
         }
         public bool Verser(Compte C, MAD montant)
         {
--- Atelier1/Atelier1/Program.cs
+++ Atelier1/Atelier1/Program.cs
@@ -16,13 +16,13 @@ namespace Atelier1
             Compte Cp2 = new Compte(new MAD(700), C2);
             Cp1.Afficher();
             Console.WriteLine("------Ajouter--------");
-            Console.WriteLine(Cp1.Créditer(new MAD(3600)));
+            Cp1.Créditer(new MAD(3600));
             Cp1.Afficher();
             Console.WriteLine("------Retier--------");
-            Console.WriteLine(Cp1.Débiter(new MAD(2600)));
+            Cp1.Débiter(new MAD(2600));
             Cp1.Afficher();
             Console.WriteLine("-------Verser-------");
-            Console.WriteLine(Cp1.Verser(Cp2, new MAD(-500)));
+            Cp1.Verser(Cp2, new MAD(-500));
             Cp1.Afficher();
             Cp2.Afficher();
             Console.ReadKey();
